cd4a5d95e5b95c35779e61de0169a6a9180dd923
que2018 <goodiswill2016@gmail.com>
2019-06-20 16:29:10 -0700

    06202019

--- src/app/views/people/men/men-edit/men-edit.component.ts
+++ src/app/views/people/men/men-edit/men-edit.component.ts
@@ -1,9 +1,11 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {fadeInOut} from '../../../animation/animation';
 import {MenService} from '../../../../services/men.service';
 
 @Component({
 	selector: 'men-edit',
-	templateUrl: 'men-edit.component.html'
+	templateUrl: 'men-edit.component.html',
+	animations: [fadeInOut]
 })
 
 export class MenEditComponent {
